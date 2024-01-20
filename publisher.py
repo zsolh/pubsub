6f8ecdc23829790fe320481a304c4c1df0afffd3
zsolh <holman.zsolt@gmail.com>
2024-01-20 21:43:16 +0100

    Update publisher.py
    adding infinite loop

--- publisher.py
+++ publisher.py
@@ -1,4 +1,5 @@
 import os
+import time
 from google.cloud import pubsub_v1
 
 credentials_path = '/home/figura/Dokumentumok/Aaron/pubsub/pp-demo.private-key.json'
@@ -16,7 +17,10 @@ attributes = {
     'nextSubscriptionName': 'projects/total-media-411521/subscriptions/MyTopic1-sub',
 }
 
-future = publisher.publish(topic_path, data, **attributes)
-print(f'published message id {future.result()}')
+while True:
+
+    future = publisher.publish(topic_path, data, **attributes)
+    print(f'published message id {future.result()}')
+    time.sleep(10)
 
 
